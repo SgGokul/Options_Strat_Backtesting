b91459c74b8c86b810a062f8477627bc38d125b1
SgGokul <sggo1405@gmail.com>
2024-05-22 13:40:01 +0530

    update trade_analysis with generic functions

--- trade_analysis.py
+++ trade_analysis.py
@@ -2,31 +2,31 @@ import pandas as pd
 import math
 
 ## calculate sharpe ratio using the return percentage
-def sharpe_ratio(df):
+def sharpe_ratio(df: pd.DataFrame) -> float:
     mean_pnl = (df['Return_pct']/100).mean()
     std_pnl = (df['Return_pct']/100).std()
     sr = math.sqrt(len(df)) * (mean_pnl/std_pnl)
     return sr
 
 ## calculate win rate/hit ratio
-def win_rate(df):
+def win_rate(df: pd.DataFrame) -> float:
     wr = (len(df[df['Abs_return'] > 0])/len(df)) * 100
     return wr 
 
 ## calculate risk to reward ratio
-def risk_reward(df):
+def risk_reward(df: pd.DataFrame) -> float:
     rr = abs((df[df['Abs_return']<0]['Abs_return']).mean()) / (df[df['Abs_return']>0]['Abs_return']).mean() 
     return rr
 
 ## calculate calmar ratio
-def calmar_ratio(df):
+def calmar_ratio(df: pd.DataFrame) -> float:
     annualized_return = df['Cumulative_Returns'].iloc[-1] ** (252 / len(df)) - 1  
     max_drawdown = (df['Drawdown'].min())/100
     calmar_ratio = annualized_return / abs(max_drawdown)
     return calmar_ratio
 
 ## generate stats sheet as needed
-def generate_stats_sheet(df, capital):
+def generate_stats_sheet(df: pd.DataFrame, capital: float) -> pd.DataFrame:
     total_trades = len(df)
     profitable_trades = df[df['P&L'] > 0]
     losing_trades = df[df['P&L'] < 0]
@@ -66,7 +66,7 @@ def generate_stats_sheet(df, capital):
     return summary_df
 
 ## calculate peak and drawdowns
-def drawdown(df):
+def drawdown(df: pd.DataFrame) -> None:
     df['Cumulative_Returns'] = (1 + df['Return_pct'] / 100).cumprod()
     df['Peak'] = df['Cumulative_Returns'].cummax()
     df['Drawdown'] = ((df['Cumulative_Returns'] - df['Peak'] ) / df['Peak'])*100
